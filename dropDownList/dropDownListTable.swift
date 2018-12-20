ad38c1250007e1f6f58ca7233934945c14f6b371
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-20 19:11:57 +0530

    done

---
+++ dropDownList/dropDownListTable.swift
@@ -0,0 +1,18 @@
+//
+//  dropDownListTable.swift
+//  dropDownList
+//
+//  Created by Mac on 20/12/18.
+//  Copyright © 2018 tops. All rights reserved.
+//
+
+import UIKit
+
+class dropDownListTable {
+
+    static let shared = dropDownListTable()
+    func test()  {
+        print("working...")
+    }
+    
+}
